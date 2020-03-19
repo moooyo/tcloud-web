8d1120b065051b0eecb356bf8a39828e5c160b9a
lengyu <lengyuchn@qq.com>
2020-03-19 22:51:28 +0800

    add upload buttion

--- src/components/fileAction.tsx
+++ src/components/fileAction.tsx
@@ -1,53 +1,65 @@
-import React from 'react'
-import {Button, Col, Input, Row} from 'antd'
-import {AppstoreOutlined, CloudDownloadOutlined, FolderAddOutlined, UploadOutlined,} from '@ant-design/icons'
-import style from './file.module.css'
+import React from 'react';
+import { Button, Col, Input, Row } from 'antd';
+import {
+  AppstoreOutlined,
+  CloudDownloadOutlined,
+  FolderAddOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import style from './file.module.css';
 
+const { Search } = Input;
 
-const {Search} = Input;
+interface props {}
 
-interface props {
-
-}
-
-interface state {
-
-}
+interface state {}
 
 class FileAction extends React.Component<props, state> {
   state = {};
 
   render() {
     return (
-      <div style={{
-        marginBottom: "0.5vh",
-        marginTop: "1vh"
-      }}>
+      <div
+        style={{
+          marginBottom: '0.5vh',
+          marginTop: '1vh',
+        }}
+      >
         <Row>
           <Col flex={2}>
-            <Button type={"primary"} icon={<UploadOutlined/>} className={style.actionButton} style={{
-              marginLeft: "22px"
-            }}>
+            <Button
+              type={'primary'}
+              icon={<UploadOutlined />}
+              className={style.actionButton}
+              style={{
+                marginLeft: '22px',
+              }}
+            >
+              <input type={'file'} style={{ opacity: '0' }} />
               上传
             </Button>
-            <Button icon={<FolderAddOutlined/>} className={style.actionButton}>
+            <Button icon={<FolderAddOutlined />} className={style.actionButton}>
               新建文件夹
             </Button>
-            <Button icon={<CloudDownloadOutlined/>} className={style.actionButton}>
+            <Button
+              icon={<CloudDownloadOutlined />}
+              className={style.actionButton}
+            >
               离线下载
             </Button>
           </Col>
           <Col flex={2}>
-          <span>
-          </span>
+            <span></span>
           </Col>
-          <Col flex={7}/>
+          <Col flex={7} />
           <Col flex={1}>
-            <div style={{
-              paddingTop: "5px"
-            }}>
+            <div
+              style={{
+                paddingTop: '5px',
+              }}
+            >
               <Search
-                placeholder={"搜索您的文件"}
+                placeholder={'搜索您的文件'}
                 onSearch={value => {
                   console.log(value);
                 }}
@@ -56,38 +68,46 @@ class FileAction extends React.Component<props, state> {
             </div>
           </Col>
           <Col>
-            <Button size={"large"} type={"link"} icon={<AppstoreOutlined/>}/>
+            <Button size={'large'} type={'link'} icon={<AppstoreOutlined />} />
           </Col>
         </Row>
       </div>
-    )
+    );
   }
 }
 
 class FileShowLoadData extends React.Component<any, any> {
   render() {
     return (
-      <div style={{
-        marginBottom: "0.5vh",
-        fontSize: "12px",
-        userSelect: "none",
-      }}>
+      <div
+        style={{
+          marginBottom: '0.5vh',
+          fontSize: '12px',
+          userSelect: 'none',
+        }}
+      >
         <Row>
-          <Col flex={1} style={{
-            textAlign: "center"
-          }}>
+          <Col
+            flex={1}
+            style={{
+              textAlign: 'center',
+            }}
+          >
             <span>全部文件</span>
           </Col>
-          <Col flex={22}/>
-          <Col flex={1} style={{
-            textAlign: "center"
-          }}>
+          <Col flex={22} />
+          <Col
+            flex={1}
+            style={{
+              textAlign: 'center',
+            }}
+          >
             <span>已经全部加载，共38个</span>
           </Col>
         </Row>
       </div>
-    )
+    );
   }
 }
 
-export {FileAction, FileShowLoadData};
+export { FileAction, FileShowLoadData };
